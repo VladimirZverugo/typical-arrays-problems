24835f2a8a7600cade31c04c84887a9b55066589
Vladimir <44143087+VladimirZverugo@users.noreply.github.com>
2021-01-23 00:52:31 +0300

    init: solution upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length) ? Math.min.apply(null, array) : 0
 }
-
 exports.max = function max (array) {
-  return 0;
-}
-
+    return (array && array.length) ?  Math.max.apply(null, array)  : 0
+};
 exports.avg = function avg (array) {
-  return 0;
-}
+    return (array && array.length) ? array.reduce((accumulator, number) => (accumulator) + (number)) / array.length : 0
+};
